ccea4cc665f7b353c6f1509334669ea1a4f7ff83
Eugene Lynch <eugene@enspiral.com>
2017-12-17 19:54:53 -0500

    can print uniq emails

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 const GoogleSpreadsheet = require('google-spreadsheet')
 const fs = require('fs')
+const uniq = require('lodash.uniq')
 const { SPREADSHEET_KEY, DONORS_SHEET_ID, VOLUNTEERS_SHEET_ID } = process.env
 
 fs.readFile('./.credentials.json', 'utf-8', (err, data) => {
@@ -16,8 +17,18 @@ fs.readFile('./.credentials.json', 'utf-8', (err, data) => {
       const { worksheets } = info
       const donorsSheet = worksheets.find(s => s.id === DONORS_SHEET_ID)
       const volunteersSheet = worksheets.find(s => s.id === VOLUNTEERS_SHEET_ID)
-      console.log(JSON.stringify(donorsSheet, null, 2))
-      console.log(JSON.stringify(volunteersSheet, null, 2))
+
+      donorsSheet.getRows((err, rows) => {
+        if (err) throw err
+        const donorEmails = uniq(rows.map(donor => donor.email))
+        console.log(donorEmails)
+      })
+
+      volunteersSheet.getRows((err, rows) => {
+        if (err) throw err
+        const volunteerEmails = uniq(rows.map(volunteer => volunteer.email))
+        console.log(volunteerEmails)
+      })
     })
   })
 })
