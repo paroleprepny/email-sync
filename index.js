b220a039ad160d036a2590534f5b74a590129066
Eugene Lynch <eugene@enspiral.com>
2018-02-11 16:59:28 -0500

    store google creds in env

--- index.js
+++ index.js
@@ -2,22 +2,18 @@
 
 require('dotenv').config()
 const GoogleSpreadsheet = require('google-spreadsheet')
-const fs = require('fs')
 const compileEmailsFromSheets = require('./compile-emails-from-sheets')
-const { SPREADSHEET_KEY } = process.env
+const { SPREADSHEET_KEY, GOOGLE_CREDENTIALS } = process.env
 
-fs.readFile('./.credentials.json', 'utf-8', (err, data) => {
-  if (err) throw err
-  const creds = JSON.parse(data)
-  const doc = new GoogleSpreadsheet(SPREADSHEET_KEY)
+const creds = JSON.parse(GOOGLE_CREDENTIALS)
+const doc = new GoogleSpreadsheet(SPREADSHEET_KEY)
 
-  doc.useServiceAccountAuth(creds, () => {
-    doc.getInfo((err, info) => {
+doc.useServiceAccountAuth(creds, () => {
+  doc.getInfo((err, info) => {
+    if (err) throw err
+    compileEmailsFromSheets(info.worksheets, (err, result) => {
       if (err) throw err
-      compileEmailsFromSheets(info.worksheets, (err, result) => {
-        if (err) throw err
-        console.log(JSON.stringify(result, null, 2))
-      })
+      console.log(JSON.stringify(result, null, 2))
     })
   })
 })
